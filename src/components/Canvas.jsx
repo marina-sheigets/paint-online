d5261ef1e6a8f7bfe78e93ca384e9adfd692827e
Marina Sheigets <marinasejgec34@gmail.com>
2023-10-16 16:48:23 +0300

    feat: add drawing figures

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,11 +1,22 @@
+import { observer } from 'mobx-react-lite';
 import '../styles/canvas.scss';
+import { useEffect, useRef } from 'react';
+import canvasState from '../store/canvasState';
+import toolState from '../store/toolState';
+import Brush from '../tools/brush';
 
-function Canvas() {
+const Canvas = observer(() => {
+	const canvasRef = useRef();
+	useEffect(() => {
+		canvasState.setCanvas(canvasRef.current);
+		toolState.setTool(new Brush(canvasRef.current));
+	}, []);
+
 	return (
 		<div className='canvas'>
-			<canvas width={600} height={400}></canvas>
+			<canvas ref={canvasRef} width={600} height={400}></canvas>
 		</div>
 	);
-}
+});
 
 export default Canvas;
--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -2,22 +2,39 @@ import '../styles/toolbar.scss';
 import { AiOutlineLine, AiOutlineSave } from 'react-icons/ai';
 import { BsFillBrushFill, BsFillSquareFill, BsCircle, BsEraserFill } from 'react-icons/bs';
 import { BiUndo, BiRedo } from 'react-icons/bi';
+import toolState from '../store/toolState';
+import Brush from '../tools/brush';
+import canvasState from '../store/canvasState';
+import Rect from '../tools/rect';
+import Circle from '../tools/circle';
+import Eraser from '../tools/eraser';
+import Line from '../tools/line';
 function ToolBar() {
 	return (
 		<div className='toolbar'>
-			<button className='toolbar__btn'>
+			<button
+				className='toolbar__btn'
+				onClick={() => toolState.setTool(new Brush(canvasState.canvas))}>
 				<BsFillBrushFill />
 			</button>
-			<button className='toolbar__btn'>
+			<button
+				className='toolbar__btn'
+				onClick={() => toolState.setTool(new Rect(canvasState.canvas))}>
 				<BsFillSquareFill />
 			</button>
-			<button className='toolbar__btn '>
+			<button
+				className='toolbar__btn '
+				onClick={() => toolState.setTool(new Circle(canvasState.canvas))}>
 				<BsCircle />
 			</button>
-			<button className='toolbar__btn'>
+			<button
+				className='toolbar__btn'
+				onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}>
 				<BsEraserFill />
 			</button>
-			<button className='toolbar__btn'>
+			<button
+				className='toolbar__btn'
+				onClick={() => toolState.setTool(new Line(canvasState.canvas))}>
 				<AiOutlineLine />
 			</button>
 			<input type='color' />
