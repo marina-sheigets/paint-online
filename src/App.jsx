901d4c7eddcfc71206d96d82d4e28031403de628
Marina Sheigets <marinasejgec34@gmail.com>
2023-10-16 21:25:41 +0300

    fix: prevent regenerate id session when copy to new tab

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import ToolBar from './components/ToolBar';
 import './styles/app.scss';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 function App() {
+	console.log('window', window.location);
 	return (
 		<BrowserRouter>
 			<div className='app'>
 				<Routes>
 					<Route path='/:id' element={<Content />}></Route>
 				</Routes>
-				<Navigate to={`f${(+new Date()).toString(16)}`} />
+				{window.location.pathname === '/' && (
+					<Navigate to={`f${(+new Date()).toString(16)}`} />
+				)}
 			</div>
 		</BrowserRouter>
 	);
