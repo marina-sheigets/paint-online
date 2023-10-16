a8340c20f9988517dbe92a90980bbcf13ee45134
Marina Sheigets <marinasejgec34@gmail.com>
2023-10-16 20:29:12 +0300

    feat: generate unique session id

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import Canvas from './components/Canvas';
 import SettingBar from './components/SettingBar';
 import ToolBar from './components/ToolBar';
 import './styles/app.scss';
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 function App() {
 	return (
-		<div className='app'>
+		<BrowserRouter>
+			<div className='app'>
+				<Routes>
+					<Route path='/:id' element={<Content />}></Route>
+				</Routes>
+				<Navigate to={`f${(+new Date()).toString(16)}`} />
+			</div>
+		</BrowserRouter>
+	);
+}
+
+function Content() {
+	return (
+		<>
 			<ToolBar />
 			<SettingBar />
 			<Canvas />
-		</div>
+		</>
 	);
 }
 
